aa815251a536a1f2c95727ca187c951534c1e0fd
Chambers <46649021+connectshark@users.noreply.github.com>
2024-08-29 16:33:59 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [vue(), VueRouter(), VueDevTools()],
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/vue-tailwind-template/'
+    : '/'
+})
