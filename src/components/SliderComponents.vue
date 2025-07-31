136cbe45c7b440d853a182a7b102741782938a0e
Chambers <bobhus394@gmail.com>
2025-07-31 23:46:14 +0800

    修改硬編碼的圖片

--- src/components/SliderComponents.vue
+++ src/components/SliderComponents.vue
@@ -2,13 +2,13 @@
   <Splide :options="{ start: startIndex }" aria-label="Our wedding photos">
     <SplideSlide v-for="photo in store.photos" :key="photo">
       <picture>
-        <source :srcset="`https://cdn.jsdelivr.net/gh/connectshark/wedding-photos@main/4x/${ photo }.webp`" media="(min-width: 1600px)">
-        <source :srcset="`https://cdn.jsdelivr.net/gh/connectshark/wedding-photos@main/2x/${ photo }.webp`" media="(min-width: 768px)">
-        <source :srcset="`https://cdn.jsdelivr.net/gh/connectshark/wedding-photos@main/1x/${ photo }.webp`" media="(max-width: 767px)">
+        <source :srcset="`${ WEDDING_PHOTO_BASE_URL }/4x/${ photo }.webp`" media="(min-width: 1600px)">
+        <source :srcset="`${ WEDDING_PHOTO_BASE_URL }/2x/${ photo }.webp`" media="(min-width: 768px)">
+        <source :srcset="`${ WEDDING_PHOTO_BASE_URL }/1x/${ photo }.webp`" media="(max-width: 767px)">
         <img loading="lazy" draggable="false"
           :style="`view-transition-name: photo-${photo};`"
           class="h-[80svh] w-3/4 mx-auto object-contain object-center"
-          :src="`https://cdn.jsdelivr.net/gh/connectshark/wedding-photos@main/1x/${ photo }.webp`"
+          :src="`${ WEDDING_PHOTO_BASE_URL }/1x/${ photo }.webp`"
           :alt="`婚紗照${photo}`"
         >
       </picture>
@@ -21,6 +21,9 @@ import { Splide, SplideSlide } from '@splidejs/vue-splide'
 import '@splidejs/vue-splide/css'
 import { usePhotosStore } from '@/stores/photo'
 import { useRoute } from 'vue-router'
+
+const WEDDING_PHOTO_BASE_URL = import.meta.env.VITE_WEDDING_PHOTO_BASE_URL
+
 const route = useRoute()
 const photoID = route.params.id
 
